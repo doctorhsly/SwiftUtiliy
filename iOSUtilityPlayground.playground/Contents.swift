226a4967fca59373e8831b319d9ff939b3f57153
doctorhsly <doctorhsly@hotmail.com>
2015-12-25 09:03:30 +0800

    Clear utility file
    Clear utility file

--- iOSUtilityPlayground.playground/Contents.swift
+++ iOSUtilityPlayground.playground/Contents.swift
@@ -2,14 +2,3 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-var str2 = "Three Branch"
-
-var str3 = "add second branch"
-
-var str4 = "add thrid branch"
-
-var str5 = "add fourth branch"
-
-var str8 = "change by jinzekid"
