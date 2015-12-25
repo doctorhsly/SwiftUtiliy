7486babbaf34cd1ea7fe4b21bfaf3ccec6bfaf29
doctorhsly <doctorhsly@hotmail.com>
2015-12-25 10:39:36 +0800

    clear
    clear

--- iOSUtilityPlayground.playground/Contents.swift
+++ iOSUtilityPlayground.playground/Contents.swift
@@ -1,13 +1,3 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-let firstName = "Tom"
-
-let lastName = "Jack"
-
-let fireName = "1111"
-
-let secondName = "2222"
-
-let thirdName = "3333"
