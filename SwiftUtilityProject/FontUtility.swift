6ee9a4ff56c5fa0f82fa66b87f9ed412a3d519fd
doctorhsly <doctorhsly@hotmail.com>
2015-12-31 09:20:43 +0800

    delete print
    delete print

--- SwiftUtilityProject/FontUtility.swift
+++ SwiftUtilityProject/FontUtility.swift
@@ -47,9 +47,7 @@ func contentSize(font: UIFont , content: String , bound:CGSize , lineBreakMode:N
     let attributes = [NSFontAttributeName:font,
         NSParagraphStyleAttributeName:paragraphStyle.copy()]
     let boundingRect = content.boundingRectWithSize(bound, options: options, attributes:attributes, context:nil)
-    print("\(boundingRect.size)")
 
-    print("test")
     return boundingRect.size
 }
 
